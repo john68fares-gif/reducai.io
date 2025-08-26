aeb04bd04ef37a7012dba3139997eafe2f93c731
john68fares-gif <john68fares@gmail.com>
2025-08-26 12:08:25 +0200

    Update voice-agent.tsx

--- pages/voice-agent.tsx
+++ pages/voice-agent.tsx
@@ -2,7 +2,7 @@
 import dynamic from 'next/dynamic';
 
 const VoiceAgentSection = dynamic(
-  () => import('@/components/voice/VoiceAgentSection'),
+  () => import('../components/voice/VoiceAgentSection'), // <-- use relative path
   { ssr: false }
 );
 
