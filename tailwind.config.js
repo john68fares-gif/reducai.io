adf6c9afb937fae3448e4c1a710cc8cb5f2d7e13
john68fares-gif <john68fares@gmail.com>
2025-08-27 14:09:05 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {
-      fontFamily: {
-        movatif: ['"Movatif Bold"', 'sans-serif'],
-        manrope: ['Manrope', 'sans-serif'],
-      },
-    },
-  },
+  theme: { extend: {} },
   plugins: [],
 };
