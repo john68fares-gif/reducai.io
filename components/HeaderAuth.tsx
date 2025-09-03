6ce8bf6f66aa1a2549e12950aea6492759e6beda
john68fares-gif <john68fares@gmail.com>
2025-09-03 16:12:19 +0200

    Update HeaderAuth.tsx

--- components/HeaderAuth.tsx
+++ components/HeaderAuth.tsx
@@ -3,7 +3,7 @@
 
 import { useEffect, useMemo, useState } from 'react';
 import Link from 'next/link';
-import { supabase } from '@/lib/supabaseClient';
+import { supabase } from '@/lib/supabase-client';
 
 const ACCENT = '#00ffc2';
 
